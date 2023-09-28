1eb3d411e2f7bf7be3f6987d664215378198cf98
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2023-09-28 16:01:45 -0600

    Add bim file as arg, make function

--- get_strand_amb_SNPs.R
+++ get_strand_amb_SNPs.R
@@ -1,6 +1,21 @@
-bim <- read.table("tmp_gwas.bim", stringsAsFactors=F)
-snps <- bim$V2[((bim$V5 == "A") & (bim$V6 == "T")) |
-	       ((bim$V5 == "T") & (bim$V6 == "A")) |
-	       ((bim$V5 == "C") & (bim$V6 == "G")) |
-	       ((bim$V5 == "G") & (bim$V6 == "C"))]
-write.table(snps, "tmp_strand_remove_snps.txt",  sep="\t", quote=F, row.names=F, col.names=F)
+#!/usr/bin/env Rscript
+
+args <- commandArgs(trailingOnly=TRUE)
+
+print("Script is called by create_initial_input.sh. First trailing")
+print("arg should be file path to .bim after crossover (if done) or")
+print("after removal of SNPs with duplicate positions. Writes new")
+print("file tmp_strand_remove_snps.txt.")
+
+get_strand_amb_SNPs <- function(bim_file) {
+  bim <- read.table(bim_file, stringsAsFactors=F)
+  snps <- bim$V2[((bim$V5 == "A") & (bim$V6 == "T")) |
+                   ((bim$V5 == "T") & (bim$V6 == "A")) |
+                   ((bim$V5 == "C") & (bim$V6 == "G")) |
+                   ((bim$V5 == "G") & (bim$V6 == "C"))]
+  path <- dirname(temp_gwas_bim)
+  write.table(x = snps, file = paste0(path, "/tmp_strand_remove_snps.txt"),
+              sep="\t", quote=F, row.names=F, col.names=F)
+}
+
+get_strand_amb_SNPs(args[1])
