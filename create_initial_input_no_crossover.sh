7837abc1e3c3cdff4385df2ac0196e20bb579fcf
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2023-09-28 17:09:45 -0600

    Add option for all chr or one chr

--- create_initial_input_no_crossover.sh
+++ create_initial_input_no_crossover.sh
@@ -3,17 +3,20 @@
 #Set arguments
 if [ "$#" -eq  "0" ]
 then
-      echo "Usage: ${0##*/} <plink_prefix> <out_dir> <code_dir>"
+      echo "Usage: ${0##*/} <plink_prefix> <out_dir> <code_dir> <chr>"
       echo "Script removes SNPs with duplicate positions, removes"
       echo "strand ambiguous SNPs, and performs pre-imputation QC."
       echo "Current script runs PLINK1.9, but can accept input of"
-      echo "PLINK1.9 or PLINK2 files."
+      echo "PLINK1.9 or PLINK2 files. If "chr" input = "all", then"
+      echo "the script will create one VCF file per chr. Otherwise,"
+      echo "must be a single chr number '1', '2', etc."
       exit
 fi
 
 plink_prefix=$1
 out_dir=$2
 code_dir=$3
+chr=$4
 
 # If plink_prefix is in PLINK2 format, convert to PLINK1.9
 if [ ! -f "$plink_prefix".fam ]
@@ -48,18 +51,32 @@ plink --bfile ${out_dir}/tmp_no_dupl \
 #missingness, SNPs not in HWE
 plink --bfile ${out_dir}/tmp_gwas_no_AT_CG \
       --maf 0.000001 --geno 0.05 --hwe 0.000001 \
-      --keep-allele-order
+      --keep-allele-order \
       --make-bed --out ${out_dir}/pre_qc
 
 #Create vcf files for uploading to imputation server for QC
 #Note that the encoding for chromosome is e.g. chr22, not chr
-for ((chr=1; chr<=22; chr++)); do
+# If chr = "all", then create one VCF file per chr, otherwise
+# chr must equal one chr number, so only make that VCF file
+if [ "$chr" == "all" ]
+then
+      for ((chr=1; chr<=22; chr++)); do
+            plink --bfile ${out_dir}/pre_qc \
+                  --chr $chr --keep-allele-order \
+                  --recode vcf --out ${out_dir}/tmp_chr${chr}
+            vcf-sort ${out_dir}/tmp_chr${chr}.vcf | \
+                  sed -E 's/^([[:digit:]]+)/chr\1/' | \
+                  bgzip -c > ${out_dir}/chr${chr}_pre_qc.vcf.gz
+      done
+else
       plink --bfile ${out_dir}/pre_qc \
             --chr $chr --keep-allele-order \
             --recode vcf --out ${out_dir}/tmp_chr${chr}
-      vcf-sort ${out_dir}/tmp_chr${chr}.vcf | \ sed -E 's/^([[:digit:]]+)/chr\1/' | \
+      vcf-sort ${out_dir}/tmp_chr${chr}.vcf | \
+            sed -E 's/^([[:digit:]]+)/chr\1/' | \
             bgzip -c > ${out_dir}/chr${chr}_pre_qc.vcf.gz
-done
+
+fi
 
 #Report SNP counts
 orig_snp_nr=`wc -l ${plink_prefix}.bim`
