ad271c076cbb0df1f60f4aeaf25d59e138eca3e4
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2024-06-17 16:22:35 -0600

    Update to also fix allele switch only case

--- fix_strands.sh
+++ fix_strands.sh
@@ -8,7 +8,7 @@ then
     echo "flips. If "chr" input = "all", then the script will create one"
     echo "VCF file per chr. Otherwise, must be a single chr number '1',"
     echo "'2', etc. Crossover is from hg19 to hg38, this script should"
-    echo "follow create_initial_input with crossover."
+    echo "follow create_initial_input (with or without crossover)."
     exit
 fi
 
@@ -23,27 +23,39 @@ Rscript --vanilla ${code_dir}/get_strand_flip_snp_names.R $pre_qc_dir $post_qc_d
 
 #Create vcf files for uploading to imputation server for QC
 #Note that the encoding for chromosome is e.g. chr22, not 22
+process_chr() {
+    chr=$1
+
+    # Flip strands for strand flip & strand flip and allele switch
+    plink --bfile ${pre_qc_dir}/pre_qc \
+        --flip ${post_qc_dir}/tmp_flip.txt \
+        --chr $chr --make-bed --keep-allele-order \
+        --out ${post_qc_dir}/tmp_chr${chr}_flip
+    # Fix allele switches after slip
+    plink --bfile ${post_qc_dir}/tmp_chr${chr}_flip \
+        --a2-allele ${post_qc_dir}/tmp_a2-allele.txt \
+        --chr $chr --make-bed --keep-allele-order \
+        --out ${post_qc_dir}/tmp_chr${chr}_flip_switch
+    # Fix allele switches only
+    plink --bfile ${post_qc_dir}/tmp_chr${chr}_flip_switch \
+        --a2-allele ${post_qc_dir}/tmp_a2-allele_switch_only.txt \
+        --chr $chr --recode vcf --keep-allele-order \
+        --out ${post_qc_dir}/tmp_chr${chr}_flip_switch_both
+
+    vcf-sort ${post_qc_dir}/tmp_chr${chr}_flip_switch_both.vcf | \
+        sed -E 's/^([[:digit:]]+)/chr\1/' | \
+        bgzip -c > ${post_qc_dir}/chr${chr}_post_qc.vcf.gz
+}
+
 # If chr = "all", then create one VCF file per chr, otherwise
 # chr must equal one chr number, so only make that VCF file
 if [ "$chr" == "all" ]
 then
     for ((chr=1; chr<=22; chr++)); do
-        plink --bfile ${pre_qc_dir}/pre_qc \
-            --flip ${post_qc_dir}/tmp_flip.txt \
-            --chr $chr --recode vcf \
-            --out ${post_qc_dir}/tmp_chr${chr}
-        vcf-sort ${post_qc_dir}/tmp_chr${chr}.vcf | \
-            sed -E 's/^([[:digit:]]+)/chr\1/' | \
-            bgzip -c > ${post_qc_dir}/chr${chr}_post_qc.vcf.gz
+        process_chr $chr
     done
 else
-    plink --bfile ${pre_qc_dir}/pre_qc \
-            --flip ${post_qc_dir}/tmp_flip.txt \
-            --chr $chr --recode vcf \
-            --out ${post_qc_dir}/tmp_chr${chr}
-        vcf-sort ${post_qc_dir}/tmp_chr${chr}.vcf | \
-            sed -E 's/^([[:digit:]]+)/chr\1/' | \
-            bgzip -c > ${post_qc_dir}/chr${chr}_post_qc.vcf.gz
+    process_chr $chr
 fi
 
 #Cleanup
--- get_strand_flip_snp_names.R
+++ get_strand_flip_snp_names.R
@@ -32,34 +32,38 @@ get_strand_flip_snp_names <- function(pre_qc_dir, post_qc_dir) {
   merge <- merge(snp.excl, bim, by = c("chr.pos"))
   
   # Get strand flips & strand flip and allele switch
-  snp.frame <- merge[grep("Strand flip", merge$FilterType),]
+  snp.frame.flip.as <- merge[grep("Strand flip", merge$FilterType),]
   
   # Remove strand flip & strand flip and allele switch from from snp.excl
-  merge.to.excl <- merge[!merge$FilterType %in% snp.frame$FilterType,]
+  merge.to.excl <- merge[!merge$FilterType %in% snp.frame.flip.as$FilterType,]
+  
+  # Get allele switch only snps
+  snp.frame.as <- merge.to.excl[grep("Allele switch", merge.to.excl$FilterType),]
   
   # Write out SNPs to be excluded using PLINK --exclude
-  merge.to.excl <- merge.to.excl[,c("V2")]
-  write.table(merge.to.excl, paste0(post_qc_dir, "/tmp_snps-excl.txt"), 
-              sep="\t", quote=F, row.names=F, col.names=F)
+  # merge.to.excl <- merge.to.excl[,c("V2")]
+  # write.table(merge.to.excl, paste0(post_qc_dir, "/tmp_snps-excl.txt"), 
+  #             sep="\t", quote=F, row.names=F, col.names=F)
   
+  ### Handle strand flips & strand flip and allele switches
   # For both strand flip & strand flip and allele switch, flip strand
   # Only do if strand flips were found, otherwise create empty list
-  if (nrow(snp.frame) > 0) {
+  if (nrow(snp.frame.flip.as) > 0) {
     # Get varIDs from .bim by chr:pos for strand flip & strand flip and
     # allele switch
-    snps <- bim$V2[bim$chr.pos %in% snp.frame$chr.pos]
+    snps <- bim$V2[bim$chr.pos %in% snp.frame.flip.as$chr.pos]
     
     # Get ref alleles for strand flip and allele switch
-    snp.frame.both <- snp.frame[grep("Strand flip and Allele switch", snp.frame$FilterType),]
-    snp.frame.both$ref <- gsub("/[[:alpha:]]", "",
-                               gsub("[[:alpha:]]+:", "", snp.frame.both$Info))
+    snp.frame.flip.as.both <- snp.frame.flip.as[grep("Strand flip and Allele switch", snp.frame.flip.as$FilterType),]
+    snp.frame.flip.as.both$ref <- gsub("/[[:alpha:]]", "",
+                               gsub("[[:alpha:]]+:", "", snp.frame.flip.as.both$Info))
     
     # Get varID name with reference a2 allele for PLINK --a2-allele
-    snp.frame.both <- snp.frame.both[,c("V2", "ref")]
+    snp.frame.flip.as.both <- snp.frame.flip.as.both[,c("V2", "ref")]
     
   } else {
     snps <- character()
-    snp.frame.both <- character()
+    snp.frame.flip.as.both <- character()
     print("No strand flip SNPs were found in the excluded SNPs.")
   }
   
@@ -70,8 +74,25 @@ get_strand_flip_snp_names <- function(pre_qc_dir, post_qc_dir) {
   
   # Write out SNPs for input into PLINK --a2-allele, will allele switch
   # strand flip and allele switch alleles after --flip
-  write.table(snp.frame.both, paste0(post_qc_dir, "/tmp_a2-allele.txt"),
+  write.table(snp.frame.flip.as.both, paste0(post_qc_dir, "/tmp_a2-allele.txt"),
               sep="\t", quote = F, row.names = F, col.names = F)
+  
+  ### Handle allele switches only
+  # Get ref alleles for strand flip and allele switch
+  snp.frame.as$ref <- gsub("/[[:alpha:]]", "",
+                           gsub("[[:alpha:]]+:", "", snp.frame.as$Info))
+  
+  # Get varID name with reference a2 allele for PLINK --a2-allele
+  snp.frame.as <- snp.frame.as[,c("V2", "ref")]
+  
+  # Write out SNPs for input into PLINK --a2-allele, will allele switch
+  # alleles switches only
+  write.table(snp.frame.as, paste0(post_qc_dir, "/tmp_a2-allele_switch_only.txt"),
+              sep="\t", quote = F, row.names = F, col.names = F)
+  
 }
 
 get_strand_flip_snp_names(args[1], args[2])
+# pre_qc_dir <- "/Users/slacksa/Library/CloudStorage/OneDrive-TheUniversityofColoradoDenver/DAISY/genetics/daisy_ask_genetics/tm_r3_imp/pre_qc"
+# post_qc_dir <- "/Users/slacksa/Library/CloudStorage/OneDrive-TheUniversityofColoradoDenver/DAISY/genetics/daisy_ask_genetics/tm_r3_imp/post_qc"
+# get_strand_flip_snp_names(pre_qc_dir, post_qc_dir)
